f052593cc5577c145dfde75d1ca44990c2a020c5
ilan <ilan@spikedata.co.za>
2020-03-06 20:56:07 +0200

    publsh.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e # Exit immediately if a command exits with a non-zero status
 
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 BASEDIR=$(dirname $0)
 cd $BASEDIR/..
 
+printf "${CYAN}npm run build${NC}\n"
 npm run build
 
 # update semver
+printf "${CYAN}npm version patch${NC}\n"
 npm version patch
 
 # git tag - `npm version` does this already
@@ -14,4 +20,13 @@ npm version patch
 # git tag $ver
 # git push --tags
 
+printf "${CYAN}npm publish${NC}\n"
 npm publish
+
+printf "${CYAN}update @spikedata/sample-simple/package.json to latest version${NC}\n"
+cd ../sample-simple
+npm install -S @spikedata/api@latest
+
+printf "${CYAN}update @spikedata/sample-/package.json to latest version${NC}\n"
+cd ../sample-web
+npm install -S @spikedata/api@latest
